1ee52f9d5e06cb282506437aa9b01c6aefe02de0
Aaron <aaron.l.france@gmail.com>
2013-03-13 00:11:40 +0100

    Removed old hpcloud files.

--- hpcloud_auth/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class AuthToken(models.Model):
-    token = models.CharField(max_length=1000)
-
-    def save(self, *args, **kwargs):
-        if len(AuthToken.objects.all()) > 0:
-            AuthToken.objects.all().delete()
-        super(AuthToken, self).save(*args,  **kwargs)
