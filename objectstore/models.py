cbafdf47b97741f2f2a3ae2a2ea81723115c4abb
AeroNotix <aaron.l.france@gmail.com>
2013-07-03 14:11:26 +0100

    Revamped the models so we can have a locally indexed cloud database.

--- objectstore/models.py
+++ objectstore/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
 class StoredObject(models.Model):
-    user = models.CharField(max_length=200)
+    container = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
     url = models.URLField(max_length=1000)
+    content_type = models.CharField(max_length=250)
+
+class KeyValue(models.Model):
+    storedobject = models.ForeignKey(
+        StoredObject
+    )
+    key = models.CharField(max_length=250)
+    value = models.TextField()
+
+    def __unicode__(self):
+        return "%s:%s" % (self.key, self.value)
