5419caa31eb4b09226b4bf6cc0b22e814831e870
AeroNotix <aaron.l.france@gmail.com>
2013-07-03 17:59:34 +0100

    Added various javascript libraries.

--- static/js/userpage.js
+++ static/js/userpage.js
@@ -3,5 +3,28 @@ function metadata(URL) {
 }
 
 function newdirectory() {
-    console.log("Create directory...");
+    $("#create_new_dir").modal();
+}
+
+function createdirectory() {
+    $.ajax({
+        type: "POST",
+        url: "/create_directory/",
+        data: {
+            directory: CONFIG.PATH + $("#directory-name-text").val()
+        },
+        success: function(data, textStatus, jqXHR) {
+            createddirectory();
+        }
+    });
+}
+
+function createddirectory() {
+    location.reload();
+}
+
+function setcookieandsubmit() {
+    $.cookie("lastfile", $("#filename").val());
+    console.log($("#filename").val());
+    console.log($("#fileupload").submit());
 }
