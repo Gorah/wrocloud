afedf753d66f1e6fcf622bbd4f6bd42cfdbbf03e
Gorah <elon.aep.delin@gmail.com>
2013-07-18 10:07:16 +0200

    fixed whitespace

--- objectstore/models.py
+++ objectstore/models.py
@@ -59,7 +59,7 @@ class MyUserManager(BaseUserManager):
 
         if not last_name:
             raise ValueError('Users must have a last name')
-        
+
         user = self.create_user(email, first_name, last_name,
             password=password,
         )
@@ -97,6 +97,7 @@ class MyUser(AbstractBaseUser):
         return self.email
 
     def has_perm(self, perm, obj=None):
+        #to implement permissions
         return True
 
     @property
